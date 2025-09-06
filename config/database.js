be7d4e8219d2998cd6f28764d42983d06e32af4a
atriyadipankar <atriyadipankar@gmail.com>
2025-09-06 17:38:02 +0530

    fix database connection

--- config/database.js
+++ config/database.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'nail-ecommerce' // Specify database name in options
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`Database: ${conn.connection.name}`);
   } catch (error) {
     console.error('Database connection error:', error);
     process.exit(1);
